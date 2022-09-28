5661f4ac9869aca6ab84b7857ec4d43bf313d126
Aayush Bhan <Bhanaayush@gmail.com>
2022-09-28 01:27:13 -0400

    Basics.java prep_1

--- src/main/java/Basics.java
+++ src/main/java/Basics.java
@@ -2,17 +2,17 @@
  * This file contains a few exercises to familiarize you with Java.
  * You should read this file from top-to-bottom. Any tasks you are to complete
  * are labelled with TODO
- *
+
  * For your convenience, we have also included references to the
  * relevant readings for each task.
  */
 
 /**
- *  1. In Java, no code can exist outside of a class. Unlike Python which uses
+ *  1. In Java, no code can exist outside a class. Unlike Python which uses
  *    functions, all code in Java uses methods.
- *
+
  *    For this exercise, all of our code is within the Basics class.
- *
+
  *    (Relevant reading: 1.1.1. Defining classes)
  */
 public class Basics {
@@ -25,10 +25,10 @@ public class Basics {
      *        public static void main(final String[] args){
      *            // Code goes in here
      *        }
-     *
+
      *    When you run this file (Basics.java), the code within the main
      *    method is run.
-     *
+
      *    (Relevant reading: 1.1.2. Defining methods)
      */
     public static void main(final String[] args) {
@@ -40,9 +40,9 @@ public class Basics {
          */
         System.out.println(7 + 5);
 
-        /* TODO (Task 1): Write a line of code below that prints the string
-         *                Hello World!
-         */
+
+        System.out.println("Hello World!");
+
 
 
 
@@ -58,10 +58,9 @@ public class Basics {
          *    (Relevant reading: 1.2. Variables and Types)
          */
 
-        /*
-         * TODO (Task 2): Create a variable named my_variable and assign it the
-         *                value 100.
-         */
+
+         int my_variable = 100;
+
 
 
 
@@ -87,12 +86,12 @@ public class Basics {
          *    (Relevant reading: 1.8.2. for Loops)
          */
 
-        /*
-         * TODO (Task 3): Create a for-loop that loops from 10 to 0, printing
-         *                the following at each iteration:
-         *                Current count: #
-         *
-         * e.g. the end result should look like:
+        for(int i = 10; i >= 0 ; i--) {
+            System.out.println("Current count: " + i);
+        }
+
+
+         /* e.g. the end result should look like:
          * Current count: 10
          * Current count: 9
          *     ...
@@ -107,24 +106,24 @@ public class Basics {
      *  6. Below is a method named split which returns a String,
      *    as indicated by the 'String' part of the header. It takes one
      *    parameter named to_split, whose type is also a String.
-     *
+
      *    This block of code above the method is called the doc comment -- this
      *    is equivalent to docstrings in Python.
-     *
+
      *    Below, we provide descriptions of what the parameters are and details
      *    about what is returned.
-     *
+
      * Create a String named 'ret' and get the first letter of
      * each word inside to_string. to_string is a String containing exactly
      * 7 words, separated by spaces.
-     *
+
      * You may want to use String.split() and String.charAt() methods.
      * See 1.5.2. APIs for their APIs.
-     *
+
      * For example, calling the below method on the string:
      *    "Idol Long Oolong Vertical Europe University Toyota"
      * should return "ILOVEUT"
-     *
+
      * (Relevant readings: 1.4. Strings and 1.4.4. StringBuilder.
      *                     1.8.2. for Loops may be useful too.)
      *
@@ -142,8 +141,9 @@ public class Basics {
          */
         StringBuilder ret = new StringBuilder();
 
-        // Fill in the rest of the body here
-
+        for (String s : to_split.split(" ")) {
+            ret.append(s.charAt(0));
+        }
         return ret.toString();
     }
 
@@ -151,25 +151,27 @@ public class Basics {
      * 7. Below is a method that takes in an integer array (similar to a list
      *    of integers). The method's name is 'oddSum' and it returns an int
      *    as indicated by the 'int' in the method header.
-     *
+
      * Return the sum of all integers at odd indices in the given array.
      * If there are no items at odd indices (e.g. a list with 0 or 1 elements),
      * return 0.
-     *
+
      * (Relevant readings: 1.6. Arrays and 1.8.2. for Loops)
      *
      * @param arr    An array of integers
      * @return       The sum of all integers at odd indices in arr
      */
     public static int oddSum(int[] arr) {
-        int current_sum = 0;
 
-        /* TODO (Task 5): Complete this method body using a for-loop.
-         *                You can find the length of an array by using the
-         *                .length attribute (e.g. arr.length)
-         *                You can index into arrays as we do in Python
-         *                (e.g. arr[i] gives you the item at index i).
-         */
+        int current_sum = 0;
+        int i = 0;
+
+        while(i < arr.length){
+            if(i % 2 == 1){
+                current_sum += arr[i];
+            }
+            i++;
+        }
 
         return current_sum;
     }
